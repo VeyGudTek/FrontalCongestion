7b4881dc290a30d67b5d97967091b9a607771f83
Tim Lin <lintim03@gmail.com>
2026-06-27 23:11:38 -0500

    Deprecate Current Implementation

--- Assets/Scripts/Rooms/Constants.cs
+++
@@ -1,6 +0,0 @@
-public static class Constants
-{
-    public const float MinDoorWidth = 1.5f;
-    public const float ZeroThreshold = .001f;
-    public const float WallThickness = 1.0f;
-}
---
+++ Assets/Scripts/RoomsDeprecated/Constants.cs
@@ -0,0 +1,6 @@
+//public static class Constants
+//{
+//    public const float MinDoorWidth = 1.5f;
+//    public const float ZeroThreshold = .001f;
+//    public const float WallThickness = 1.0f;
+//}
